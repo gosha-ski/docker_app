b4a4798a65ee3858de3c763f9e276ba333a391aa
gosha-ski <yaren042@mail.ru>
2025-08-25 19:32:04 +0500

    wokflow

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/docker", (request, response) => {
 	response.send("okey docker");
 });
 
+app.get("/remote", (request, response) => {
+	response.send("Hello world");
+});
+
 app.listen(8080, () => {
 	console.log("server works");
 });
